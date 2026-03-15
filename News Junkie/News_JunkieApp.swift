d8f8e4f1d1bb49770178f58ca4e9dcca94a9d6e5
Chris Lane <clane86@gmail.com>
2026-03-15 16:19:28 -0400

    Initial Commit

---
+++ News Junkie/News_JunkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_JunkieApp.swift
+//  News Junkie
+//
+//  Created by Chris Lane on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct News_JunkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
